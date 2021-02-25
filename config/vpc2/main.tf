424467adb84517e3b329c740e5441aca90c85235
urvashijain18 <jain.ur@husky.neu.edu>
2021-02-25 13:43:39 -0500

    multiple vpcs code

---
+++ config/vpc2/main.tf
@@ -0,0 +1,10 @@
+module "vpc2" {
+    source = "../../modules/vpc"
+    aws_region = "${var.aws_region}"
+    aws_vpc_cidr_block = "${var.aws_vpc_cidr_block}"
+    aws_subnet1_cidr = "${var.aws_subnet1_cidr}"
+    aws_subnet2_cidr = "${var.aws_subnet2_cidr}"
+    aws_subnet3_cidr = "${var.aws_subnet3_cidr}"
+    aws_vpc_name = "${var.aws_vpc_name}"
+    aws_route_gateway_destination_cidr_block = "${var.aws_route_gateway_destination_cidr_block}"
+}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+    default = "us-east-1"
+}
+
+variable "aws_vpc_name" {
+    default = "csye6225"
+}
+
+variable "aws_vpc_cidr_block" {
+    default = "190.160.0.0/16"
+}
+
+variable "aws_subnet1_cidr"{
+  default = "190.160.1.0/24"
+}
+
+variable "aws_subnet2_cidr"{
+  default = "190.160.2.0/24"
+}
+
+variable "aws_subnet3_cidr"{
+  default = "190.160.3.0/24"
+}
+
+variable "aws_route_gateway_destination_cidr_block"{
+  default = "0.0.0.0/0"
+}
+
+
